e83c135694d3317d678a738e18c72873e0eda18f
jonathanhamler <jkhamler@gmail.com>
2018-08-09 11:47:18 +0100

    Entities, properties, relations, fixtures...

--- src/Entity/LeagueTeam.php
+++ src/Entity/LeagueTeam.php
@@ -73,4 +73,74 @@ class LeagueTeam
         return $this;
     }
 
+    /**
+     * @return int
+     */
+    public function getGoalsScored() : int{
+        $goalsScored = 0;
+
+        foreach ($this->getLeague()->getMatches() as $leagueMatch) {
+            $goalsScored+= $leagueMatch->getHomeTeamGoalsScored();
+        }
+
+        return $goalsScored;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGoalsConceded() : int{
+        $goalsScored = 0;
+
+        foreach ($this->getLeague()->getMatches() as $leagueMatch) {
+            $goalsScored+= $leagueMatch->getAwayTeamGoalsScored();
+        }
+
+        return $goalsScored;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYellowCardsReceived() : int{
+        $yellowCardsReceived = 0;
+
+        foreach ($this->getLeague()->getMatches() as $leagueMatch) {
+            $yellowCardsReceived+= $leagueMatch->getHomeTeamYellowCards();
+        }
+
+        return $yellowCardsReceived;
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getRedCardsReceived() : int{
+        $redCardsReceived = 0;
+
+        foreach ($this->getLeague()->getMatches() as $leagueMatch) {
+            $redCardsReceived+= $leagueMatch->getHomeTeamRedCards();
+        }
+
+        return $redCardsReceived;
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getLeaguePoints() : int{
+
+        $leaguePoints = 0;
+
+        $leaguePoints += ($this->getGoalsScored() * 3); // 3 points per goal scored
+        $leaguePoints -= ($this->getGoalsConceded()); // -1 point per goal conceded
+
+        $leaguePoints -= ($this->getYellowCardsReceived() * 2); // -2 points per yellow card received
+        $leaguePoints -= ($this->getRedCardsReceived() * 10); // -10 points per red card received
+
+        return $leaguePoints;
+    }
+
 }
